e3c9c93be2198fe13a68b9af6bb585245479f926
l3lackShark <lea2616@gmail.com>
2021-07-27 20:10:27 +0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/l3lackShark/winservicerunner
+
+go 1.16
+
+require (
+	github.com/kardianos/service v1.2.0
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c
+)
---
+++ main.go
@@ -0,0 +1,220 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+
+	"github.com/kardianos/service"
+	"golang.org/x/sys/windows"
+)
+
+const processEntrySize = 568
+
+func getProcessID(name string) (uint32, error) {
+	h, e := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
+	if e != nil {
+		return 0, e
+	}
+	p := windows.ProcessEntry32{Size: processEntrySize}
+	for {
+		e := windows.Process32Next(h, &p)
+		if e != nil {
+			return 0, e
+		}
+		if windows.UTF16ToString(p.ExeFile[:]) == name {
+			return p.ProcessID, nil
+		}
+	}
+}
+
+// Config is the runner app config structure.
+type Config struct {
+	Name, DisplayName, Description string
+
+	Dir  string
+	Exec string
+	Args []string
+	Env  []string
+
+	Stderr, Stdout string
+}
+
+var logger service.Logger
+
+type program struct {
+	exit    chan struct{}
+	service service.Service
+
+	*Config
+
+	cmd *exec.Cmd
+}
+
+func (p *program) Start(s service.Service) error {
+	// Look for exec.
+	// Verify home directory.
+	fullExec, err := exec.LookPath(p.Exec)
+	if err != nil {
+		return fmt.Errorf("failed to find executable %q: %v", p.Exec, err)
+	}
+
+	p.cmd = exec.Command(fullExec, p.Args...)
+	p.cmd.Dir = p.Dir
+	p.cmd.Env = append(os.Environ(), p.Env...)
+
+	go p.run()
+	return nil
+}
+func (p *program) run() {
+	logger.Info("Starting ", p.DisplayName)
+	defer func() {
+		if service.Interactive() {
+			p.Stop(p.service)
+		} else {
+			p.service.Stop()
+		}
+	}()
+
+	if p.Stderr != "" {
+		//remove old file
+		os.Remove(p.Stderr)
+
+		f, err := os.OpenFile(p.Stderr, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+		if err != nil {
+			logger.Warningf("Failed to open std err %q: %v", p.Stderr, err)
+			return
+		}
+		defer f.Close()
+		p.cmd.Stderr = f
+	}
+	if p.Stdout != "" {
+
+		os.Remove(p.Stdout)
+		f, err := os.OpenFile(p.Stdout, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0777)
+		if err != nil {
+			logger.Warningf("Failed to open std out %q: %v", p.Stdout, err)
+			return
+		}
+		defer f.Close()
+		p.cmd.Stdout = f
+	}
+
+	err := p.cmd.Run()
+	if err != nil {
+		logger.Warningf("Error running: %v", err)
+	}
+}
+func (p *program) Stop(s service.Service) error {
+	logger.Info("Stopping ", p.DisplayName)
+	if p.cmd.Process != nil {
+		p.cmd.Process.Kill()
+		pid, err := getProcessID("winservice.exe")
+		if err != nil {
+			logger.Info("The process is not running")
+		}
+		kill := exec.Command("taskkill", "/T", "/F", "/PID", strconv.Itoa(int(pid)))
+		err = kill.Run()
+		if err != nil {
+			logger.Error("failed to kill the service")
+		}
+	}
+	if service.Interactive() {
+		os.Exit(0)
+	}
+	return nil
+}
+
+func getConfigPath() (string, error) {
+	fullexecpath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+
+	dir, execname := filepath.Split(fullexecpath)
+	ext := filepath.Ext(execname)
+	name := execname[:len(execname)-len(ext)]
+
+	return filepath.Join(dir, name+".json"), nil
+}
+
+func getConfig(path string) (*Config, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	conf := &Config{}
+
+	r := json.NewDecoder(f)
+	err = r.Decode(&conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+func main() {
+	svcFlag := flag.String("service", "", "Control the system service.")
+	flag.Parse()
+
+	configPath, err := getConfigPath()
+	if err != nil {
+		log.Fatal(err)
+	}
+	config, err := getConfig(configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	svcConfig := &service.Config{
+		Name:        config.Name,
+		DisplayName: config.DisplayName,
+		Description: config.Description,
+	}
+
+	prg := &program{
+		exit: make(chan struct{}),
+
+		Config: config,
+	}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	prg.service = s
+
+	errs := make(chan error, 5)
+	logger, err = s.Logger(errs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		for {
+			err := <-errs
+			if err != nil {
+				log.Print(err)
+			}
+		}
+	}()
+
+	if len(*svcFlag) != 0 {
+		err := service.Control(s, *svcFlag)
+		if err != nil {
+			log.Printf("Valid actions: %q\n", service.ControlAction)
+			log.Fatal(err)
+		}
+		return
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+}
